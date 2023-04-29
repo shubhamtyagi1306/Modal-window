ef17d3cb4ecbf213aaae7395345f6ff25db049ea
Shubham Tyagi <shubhamtyagi1306@gmail.com>
2023-04-30 01:11:27 +0530

    create function for click events and remove hidden class

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener("click", function () {
+    console.log("Button clicked");
+    modal.classList.remove("hidden");
+  });
+}
