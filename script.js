39940bc58e89f2a1d4b815bac84dca48a96b4ddf
Shubham Tyagi <shubhamtyagi1306@gmail.com>
2023-04-30 12:56:40 +0530

    create a click event for esc key to close the modal window when pressed

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
 
 const openModal = function () {
-  console.log("Button clicked");
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -25,3 +24,11 @@ for (let i = 0; i < btnsShowModal.length; i++) {
 btnCloseModal.addEventListener("click", closeModal);
 
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+
+  if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
